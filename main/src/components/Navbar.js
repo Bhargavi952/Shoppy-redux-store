1b8cbd9334c833bddd47e69996e7b1c0bde21076
Bhargavi <bhargavishetty113@gmail.com>
2021-08-29 16:51:35 +0530

    Navbar added

---
+++ main/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>Shoppy</h2>
+      </div>
+    </div>
+    )
+}
+
+export default Navbar
